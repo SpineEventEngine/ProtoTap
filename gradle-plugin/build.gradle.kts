8ae379a4f2967d09b5d55afb017ea7dda1e8dce8
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-04-27 22:02:43 +0100

    Fix reference from the plugin marker artifact
    Also:
     * Remove unnecessary test suite. We're going to use `test` only.
     * Ensure task dependencies (just in case).

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -63,18 +63,7 @@ testing {
                 implementation(Kotlin.gradlePluginLib)
                 implementation(gradleKotlinDsl())
                 implementation(Protobuf.GradlePlugin.lib)
-//                implementation(Spine.pluginTestlib)
-            }
-        }
-
-        val functionalTest by registering(JvmTestSuite::class) {
-            useJUnitJupiter(JUnit.version)
-            dependencies {
-                implementation(Kotlin.gradlePluginLib)
-                implementation(Kotlin.testJUnit5)
-//                implementation(Spine.testlib)
-//                implementation(Spine.pluginTestlib)
-                implementation(project(":gradle-plugin"))
+                implementation(Spine.pluginTestlib)
             }
         }
     }
@@ -84,7 +73,7 @@ testing {
  * Make functional tests depend on publishing all the submodules to Maven Local so that
  * the Gradle plugin can get all the dependencies when it's applied to the test projects.
  */
-val functionalTest: Task by tasks.getting {
+val test: Task by tasks.getting {
     val task = this
     rootProject.subprojects.forEach { subproject ->
         task.dependsOn(":${subproject.name}:publishToMavenLocal")
@@ -112,10 +101,6 @@ gradlePlugin {
             tags.set(listOf("protobuf", "protoc", "codegen", "gradle", "plugin"))
         }
     }
-    val functionalTest by sourceSets.getting
-    testSourceSets(
-        functionalTest
-    )
 }
 
 // Add the common prefix to the `pluginMaven` publication.
@@ -140,6 +125,21 @@ project.afterEvaluate {
 
         // Do not add the prefix for the publication which produces
         // the `io.spine.prototap.gradle.plugin` marker.
+
+        publications.withType<MavenPublication>().configureEach {
+            if (name.endsWith("PluginMarkerMaven")) {
+                pom.withXml {
+                    asNode().apply {
+                        val root = asElement()
+                        // Get the `dependencies/dependency/artifactId` node.
+                        // It's the second node with such a name from the top.
+                        // The first one is the `artifactId` of the marker itself.
+                        val artifactIdNode = root.getElementsByTagName("artifactId").item(1)
+                        artifactIdNode.textContent = "$prefix${project.name}"
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -169,3 +169,5 @@ tasks {
         notCompatibleWithConfigurationCache("https://github.com/gradle/gradle/issues/21283")
     }
 }
+
+configureTaskDependencies()
