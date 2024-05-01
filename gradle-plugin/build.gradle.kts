cd47f0a67036775fec76202eb6e3ebb75a0a69d7
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-05-01 16:06:52 +0100

    Remove forbidden tags
    ... as advised by this server response:
    
    ```
    Caused by: java.lang.RuntimeException: Failed to post to server.
    Server responded with:
    The provided tag(s) 'gradle', 'plugin' are forbidden. All forbidden tags are: 'gradle', 'plugin'.
    	at com.gradle.publish.ResponseUtil.assertValidResponse(ResponseUtil.java:17)
    	at com.gradle.publish.OAuthHttpClient.send(OAuthHttpClient.java:80)
    	at com.gradle.publish.PortalPublisher.doSignedPost(PortalPublisher.java:92)
    	at com.gradle.publish.PortalPublisher.publishToPortal(PortalPublisher.java:58)
    	at com.gradle.publish.PublishTask.executeTask(PublishTask.java:90)
    ```
    
    For details see: https://github.com/SpineEventEngine/ProtoTap/actions/runs/8910711476/job/24470499713

--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -144,7 +144,7 @@ gradlePlugin {
             implementationClass = "io.spine.tools.prototap.gradle.Plugin"
             displayName = "ProtoTap Gradle Plugin"
             description = "Obtains generated code and related data from Protobuf compiler"
-            tags.set(listOf("protobuf", "protoc", "prototap", "codegen", "gradle", "plugin"))
+            tags.set(listOf("protobuf", "protoc", "prototap", "codegen"))
         }
     }
 }
