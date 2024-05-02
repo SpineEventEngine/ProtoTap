0ad4abe756720c4cec60b70026cebc7f6afe0b0a
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-05-02 21:52:38 +0100

    Remove commented out dependency

--- buildSrc/src/main/kotlin/module.gradle.kts
+++ buildSrc/src/main/kotlin/module.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023, TeamDev. All rights reserved.
+ * Copyright 2024, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,8 +89,6 @@ fun Module.addDependencies() {
         compileOnlyApi(FindBugs.annotations)
         compileOnlyApi(CheckerFramework.annotations)
         ErrorProne.annotations.forEach { compileOnlyApi(it) }
-
-//        implementation(Guava.lib)
 
         testImplementation(Guava.testLib)
         JUnit.api.forEach { testImplementation(it) }
