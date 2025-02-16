1438d9a71ced89cbda11760332923bd2f0fb1270
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-02-16 16:20:29 +0000

    Fix file layout

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,3 @@
-import io.spine.dependency.local.Base
-
 /*
  * Copyright 2025, TeamDev. All rights reserved.
  *
@@ -25,6 +23,8 @@ import io.spine.dependency.local.Base
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+import io.spine.dependency.local.Base
 
 dependencies {
     implementation(Base.lib)
